8ccd94d1f9a30ff4753915314b9846b1e2c2f8fb
mayaitz <mayait55@gmail.com>
2024-06-06 18:11:05 +0300

    feat(UserRouter.ts): add routing option for user handling

---
+++ server/src/Routes/UserRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router()
+
+router.post("/register", (req, res) => {});
+router.get("/login", (req, res) => {});
+router.patch("/edit", (req, res) => {});
+
+export default router;

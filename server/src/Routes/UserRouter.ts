644dadcd7deebb25382190a465f3de0463218301
mayaitz <mayait55@gmail.com>
2024-06-09 15:52:15 +0300

    feat(UserRouter.ts): add missing routing options for address and credit card

--- server/src/Routes/UserRouter.ts
+++ server/src/Routes/UserRouter.ts
@@ -8,5 +8,9 @@ router.get("/:userID/orders", (req, res) => {});
 router.get("/cart", (req, res) => {});
 router.post("/cart", (req, res) => {});
 router.delete("/cart/:itemID", (req, res) => {});
+router.post("/:userID/credit", (req, res) => {});
+router.post("/:userID/address", (req, res) => {});
+router.delete("/userID/credit", (req, res) => {});
+router.delete("/userID/address", (req, res) => {});
 
 export default router;

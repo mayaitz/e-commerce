5a99b2faf89baa178c2518be43bab3f6138fc74a
mayaitz <mayait55@gmail.com>
2024-06-09 12:00:24 +0300

    feat(ProductRouter.ts, UserRouter.ts): add missing routes

--- server/src/Routes/ProductRouter.ts
+++ server/src/Routes/ProductRouter.ts
@@ -1,8 +1,11 @@
 import express from "express";
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {});
 router.get("/:productID", (req, res) => {});
 router.get("/filtered", (req, res) => {});
+router.patch("/:productID/edit", (req, res) => {});
+router.delete("/:productID", (req, res) => {});
+router.post("/", (req, res) => {});
 
 export default router;
--- server/src/Routes/UserRouter.ts
+++ server/src/Routes/UserRouter.ts
@@ -1,8 +1,12 @@
 import express from "express";
-const router = express.Router()
+const router = express.Router();
 
 router.post("/register", (req, res) => {});
 router.get("/login", (req, res) => {});
-router.patch("/edit", (req, res) => {});
+router.patch("/:userID/edit", (req, res) => {});
+router.get("/:userID/orders", (req, res) => {});
+router.get("/cart", (req, res) => {});
+router.post("/cart", (req, res) => {});
+router.delete("/cart/:itemID", (req, res) => {});
 
 export default router;

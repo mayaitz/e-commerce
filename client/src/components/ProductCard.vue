39a7579936ce7b16235b8207572d1d290fd7aa68
mayaitz <mayait55@gmail.com>
2024-06-10 16:51:26 +0300

    feat(App.vue, ProductCard.vue, Home.vue): design the home page

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="image" class="card-img-top" />
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ description }}</p>
+      <a v-if="isInCart" @click="$emit('addToCart')" class="btn btn-warning"
+        >add to cart</a
+      ><a v-else @click="$emit('removeFromCart')" class="btn btn-warning"
+        >remove from cart</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["image", "title", "description", "isInCart"],
+};
+</script>
+
+<style>
+.card {
+  margin: 3rem;
+  max-width: 300px;
+}
+
+img {
+  max-height: 200px;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,33 @@
-<template><div></div></template>
+<template>
+  <div>
+    <div class="d-flex" :v-for="product in products">
+      <product-card-vue
+        class="p-2 flex-fill"
+        image="https://m.media-amazon.com/images/I/515sXTO5QkL._AC_UF1000,1000_QL80_.jpg"
+        title="cool pen"
+        description="Some quick example of a pen. an exquisite pen."
+        :is-in-cart="isInCart"
+        @addToCart="isInCart = !isInCart"
+        @removeFromCart="isInCart = !isInCart"
+      ></product-card-vue>
+    </div>
+  </div>
+</template>
 
 <script>
+import ProductCardVue from "@/components/ProductCard.vue";
 // @ is an alias to /src
 
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    ProductCardVue,
+  },
+  data() {
+    return {
+      products: [1, 2, 3],
+      isInCart: true,
+    };
+  },
 };
 </script>

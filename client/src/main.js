ab9971ff71ed83a0f9e208da0df359a9cafcb1f2
mayaitz <mayait55@gmail.com>
2024-06-13 17:10:47 +0300

    chore(main.js, package.json): add font awesome

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/routes";
 import store from "./store/store";
-
 import "bootstrap/dist/css/bootstrap.css";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
